9d559b17437eca55494c61bdebde7703b766223d
Jorge Garcia <jorgealfonsogarcia@gmail.com>
2022-08-28 10:39:11 -0500

    :bug: (fix) Added Thymeleaf and chaned config. to fix HTTP 404.

--- ProfileMicroserviceConsumer/src/main/java/com/jcombat/client/ProfileController.java
+++ ProfileMicroserviceConsumer/src/main/java/com/jcombat/client/ProfileController.java
@@ -3,29 +3,35 @@ package com.jcombat.client;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequestMapping("/")
 public class ProfileController {
-	
-	@Autowired
-	ProfileRepository profileRepository;
-	
-	@RequestMapping("/")
-	public String home(){
-		return "index";
-	}
-	
-	@RequestMapping("/userProfiles")
-	public String profileList(Model model) {
-		model.addAttribute("profiles", profileRepository.getAllProfiles());
-		return "userProfiles";
-	}
-	
-	@RequestMapping("/userDetails")
-	public String profileDetails(@RequestParam("id") String userId, Model model) {
-		model.addAttribute("profile", profileRepository.getProfile(userId));
-		return "userDetails";
-	}
+
+    private final ProfileRepository profileRepository;
+
+    @Autowired
+    public ProfileController(ProfileRepository profileRepository) {
+        this.profileRepository = profileRepository;
+    }
+
+    @RequestMapping
+    public String home() {
+        return "index";
+    }
+
+    @RequestMapping("/user/profiles")
+    public String profileList(Model model) {
+        model.addAttribute("profiles", profileRepository.getAllProfiles());
+        return "user-profiles";
+    }
+
+    @RequestMapping("/user/details/{id}")
+    public String profileDetails(@PathVariable("id") String userId, Model model) {
+        model.addAttribute("profile", profileRepository.getProfile(userId));
+        return "user-details";
+    }
 }
